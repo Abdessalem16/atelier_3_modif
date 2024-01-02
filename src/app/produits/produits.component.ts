dea55d6ebfab5040c14e5a582fa7b40821bc91ca
abdessalem <abdessalem461997@gmail.com>
2024-01-02 14:09:11 +0100

    Add new files and modify existing ones

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
+})
+export class ProduitsComponent {
+ produits : string[];
+ constructor(){
+  this.produits=["PC Asus","Imprimante Epson","Tablette Samsung"];
+ }
+}
